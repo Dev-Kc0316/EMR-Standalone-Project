148163a3e8d7c4bffb14f8fb56559ee5737a70d2
voidmimi <biyibioluwatosin@gmail.com>
2026-07-30 15:05:29 +0100

    added redis, cors and set up app.js basics

--- src/app.js
+++ src/app.js
@@ -1,5 +1,37 @@
+import express from "express";
+import dotenv from "dotenv";
+import { serverErrorHandler } from "./middlewares/errorMiddleware.js";
+
 import { connectDB } from "./config/database.js";
 
-console.log("Hello dont you see me running");
+dotenv.config();
 
-connectDB();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to our EMR Project");
+});
+app.use((req, res) => {
+  res.status(404).json({
+    status: "failed",
+    message: "Route not found",
+  });
+});
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port:http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    serverErrorHandler(error);
+  }
+};
+
+startServer();
